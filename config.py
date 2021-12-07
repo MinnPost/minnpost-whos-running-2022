4bf536860e2962ed9e88ef11c7fb55185183bf8c
Jonathan Stegall <jstegall@minnpost.com>
2021-12-07 08:33:39 -0600

    config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    BASE_DIR = Path(__file__).parent.parent
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    CACHE_TYPE = os.environ.get("CACHE_TYPE", "redis")
+    CACHE_REDIS_HOST = os.environ.get("CACHE_REDIS_HOST", "redis")
+    CACHE_REDIS_PORT = os.environ.get("CACHE_REDIS_PORT", 6379)
+    CACHE_REDIS_DB = os.environ.get("CACHE_REDIS_DB", 0)
+    CACHE_REDIS_URL = os.environ.get("CACHE_REDIS_URL", "redis://127.0.0.1:6379/0")
+    CACHE_DEFAULT_TIMEOUT = os.environ.get("CACHE_DEFAULT_TIMEOUT", 500)
