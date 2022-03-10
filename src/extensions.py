51330111100b19d04f3b33f8c12f14e7cc35465c
Jonathan Stegall <jstegall@minnpost.com>
2022-03-10 12:14:35 -0600

    rebuild the pipenv

--- src/extensions.py
+++ src/extensions.py
@@ -1,8 +1,5 @@
-from src.cache import cache
 from src.logger import TrackerLogger
 
 def register_extensions(app):
-
-    cache.init_app(app)
 
     app.log = TrackerLogger('tracker_results').logger
